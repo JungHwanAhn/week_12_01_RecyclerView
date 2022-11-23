d2c2b9812b01fcfe7126fbc24466e3ed7acffd2f
JungHwanAhn <wjdghks1831@naver.com>
2022-11-23 14:41:43 +0900

    🚀 RecyclerView

---
+++ app/src/main/java/kr/ac/kumoh/s20180668/week_12_01_/ListViewModel.kt
@@ -0,0 +1,26 @@
+package kr.ac.kumoh.s20180668.week_12_01_
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ListViewModel: ViewModel() {
+    private val songs = ArrayList<String>()
+    private val _list = MutableLiveData<ArrayList<String>>()
+    val list: LiveData<ArrayList<String>>
+    get() = _list
+
+    init {
+        _list.value = songs
+    }
+
+    //fun getList(): LiveData<ArrayList<String>> = list
+
+    fun add(song: String) {
+        songs.add(song)
+        _list.value = songs
+    }
+
+    //fun getSong(i: Int) = songs[i]
+    //fun getSize() = songs.size
+}
